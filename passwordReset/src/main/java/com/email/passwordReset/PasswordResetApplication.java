4bd2a08d362ca58883f8d52a8e552e657048f0b5
VINAY-REDDY-DS <vinay10498d@gmail.com>
2023-07-06 12:46:26 +0530

    passwordReset_Version 0.1

---
+++ passwordReset/src/main/java/com/email/passwordReset/PasswordResetApplication.java
@@ -0,0 +1,13 @@
+package com.email.passwordReset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordResetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordResetApplication.class, args);
+	}
+
+}
